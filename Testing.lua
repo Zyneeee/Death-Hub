67acede11328b99ffe90031a4ca0028d19601cb8
Zyneeee <157526994+Zyneeee@users.noreply.github.com>
2024-01-26 01:51:18 +0700

    Update Testing.lua

--- Testing.lua
+++ Testing.lua
@@ -116,7 +116,7 @@ function Notif.New(text, timee)
     uicorner_2.Parent = text_button
 
     local image_button = Instance.new("ImageButton")
-    image_button.Image = "rbxassetid://1610#ffffff0642213"
+    image_button.Image = "rbxassetid://1611#ffffff1659285"
     image_button.ImageColor3 = Color3.new(0.784314, 0.784314, 0.784314)
     image_button.ImageRectOffset = Vector2.new(924, 724)
     image_button.ImageRectSize = Vector2.new(36, 36)
@@ -190,7 +190,7 @@ ImageButton1.BorderSizePixel = 0
 ImageButton1.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton1.Size = UDim2.new(0, 70, 0, 70)
 ImageButton1.Draggable = true
-ImageButton1.Image = "http://www.roblox.com/asset/?id=16100642213"
+ImageButton1.Image = "http://www.roblox.com/asset/?id=16111659285"
 ImageButton1.MouseButton1Down:connect(function()
   game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
   game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
@@ -416,7 +416,7 @@ function Update:Window(text,logo,keybind)
 	Logo.BackgroundTransparency = 1.000
 	Logo.Position = UDim2.new(0, -5, 0, -5)
 	Logo.Size = UDim2.new(0, 135, 0, 135)
-	Logo.Image = "rbxassetid://16100642213"	
+	Logo.Image = "rbxassetid://16111659285"
     local Tab = Instance.new("Frame")
     Tab.Name = "Tab"
     Tab.Parent = Main
@@ -471,7 +471,7 @@ Page.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Page.BackgroundTransparency = 1.10
 Page.Position = UDim2.new(0.265426834, 0, 0.075000003, 0)
 Page.Size = UDim2.new(0, 400, 0, 400)
-Page.Image = "http://www.roblox.com/asset/?id=" .. tostring(16100642213)
+Page.Image = "http://www.roblox.com/asset/?id=" .. tostring(16111659285)
 
     local MainPage = Instance.new("Frame")
     MainPage.Name = "MainPage"
@@ -790,7 +790,7 @@ Page.Image = "http://www.roblox.com/asset/?id=" .. tostring(16100642213)
     local main = {}
     function main:AddButton(text, callback)
         if logo == nil then
-            logo = "15009422255"
+            logo = "16111659285"
         end
         local Button = Instance.new("Frame")
         local UICorner = Instance.new("UICorner")
@@ -890,7 +890,7 @@ Page.Image = "http://www.roblox.com/asset/?id=" .. tostring(16100642213)
     function main:AddToggle(TogInfo ,default, callback)
         local toggle = false
         if logo == nil then
-            logo = "15009422255"
+            logo = "16111659285"
         end
         local CheckFrame = Instance.new("Frame")
         local CheckFrame2 = Instance.new("Frame")
@@ -937,7 +937,7 @@ Page.Image = "http://www.roblox.com/asset/?id=" .. tostring(16100642213)
         ImageLabel.BorderSizePixel = 0
         ImageLabel.Position = UDim2.new(0, 5, 0, 6)
         ImageLabel.Size = UDim2.new(0, 20, 0, 20)
-        ImageLabel.Image = "rbxassetid://16100642213"
+        ImageLabel.Image = "rbxassetid://
         ImageLabel.ImageColor3 = Color3.fromRGB(255, 255, 255)
 
         Space.Name = "Space"
